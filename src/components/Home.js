e591fa5f5f457957a5efc7b30bc882c6ba238eb6
Thiemann96 <erictg96@googlemail.com>
2018-07-22 19:36:21 +0200

    hotfix

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,28 +1,4 @@
 import React from 'react';
-import Carousel from 'nuka-carousel'
-import { parseTwoDigitYear } from '../../../../../.cache/typescript/2.9/node_modules/moment';
-
-
-  /* 
-    Beide JSON Einträge müssen richtig geparst werden. Zusätzlich muss in Python noch 
-    die Datei erheblich verkleinert werden(letzte 60 Messwerte oder sowas wäre denkbar)
-    + Weiter anschauen das Dokument möglichst gut fürs FrontEnd zuzuschneien.(done)
-    + Parameterübergabe hinkriegen (halfway done,erstmalige ausführung des skripts beim start vom server
-                                      verhindern)
-    + Parameterbestimmung => ID mit geograph gleichsetzen und vergleichen welche ID am nähsten ist (done)
-    + Einpflegung der DWD Daten in einen Plot der gleichzusetzen mit denen von der senseBox ist 
-        + Datendownload abhängig von dem Phänomen 
-    + Weiter nach Statistik Tool umschauen 
-    + Recherche weiterer Daten(=>LANUV?)
-    + Ordnerstruktur für py skripts überlegen(ins
-     ++++ Stations.csv neu aufbereiten  
-      
-      
-      
-    . wo werden die .zips gespeichert?)
-  */
-
-
 class Home extends React.Component{
     constructor(props){
         super(props)
