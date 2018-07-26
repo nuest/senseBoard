0a5c8966024cb9ab6610b24df90f35dcfe42de68
Thiemann96 <erictg96@googlemail.com>
2018-07-26 22:27:47 +0200

    Component now only rerenders the playground section w/o the input area

--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -1,21 +1,45 @@
 import React from 'react'
 import Stats from './Stats'
 import Loading from './Loading'
+import SvgIcon from 'react-icons-kit';
+import {ic_date_range} from 'react-icons-kit/md/ic_date_range'
+import {ic_router} from 'react-icons-kit/md/ic_router'
+import {ic_refresh} from 'react-icons-kit/md/ic_refresh'
+import {ic_cloud_queue} from 'react-icons-kit/md/ic_cloud_queue'/* gets called from statistics with props : data */
+import {ic_brush} from 'react-icons-kit/md/ic_brush'
+import domtoimage from 'dom-to-image'
+
 class Statistics extends React.Component{
 
     constructor(props){
         super(props)
-        console.log(props)
+        var to = new Date()
+        to = to.toISOString().substring(0,10)
+        var from = new Date()
+        from.setMonth(from.getMonth()-1)
+        from = from.toISOString().substring(0,10)
+
         this.state={
             loading:true,
             title:null,
             url:null,
-            senseBoxData:null
+            senseBoxData:null,
+            senseBoxID:null,
+            phenomenon:null,
+            from:from,
+            to:to,
+            window:3600000,
         }
 
         this.handleChangeStart = this.handleChangeStart.bind(this);
         this.handleChangeEnd = this.handleChangeEnd.bind(this);
         this.fetchBox = this.fetchBox.bind(this)
+        this.updateInputId = this.updateInputId.bind(this)
+        this.updateInputPhenom = this.updateInputPhenom.bind(this)
+        this.updateInputFrom = this.updateInputFrom.bind(this)
+        this.updateInputTo = this.updateInputTo.bind(this)
+        this.updateInputWindow = this.updateInputWindow.bind(this)
+        this.handleClick = this.handleClick.bind(this)
     }//End constructor
 
     handleChangeStart(date) {
@@ -28,6 +52,13 @@ class Statistics extends React.Component{
             endDate: date
         });
     }
+
+    downloadFile(){
+        var node = document.getElementById('story');
+        domtoimage.toJpeg(document.getElementById('story'), { quality: 0.95 ,bgcolor:'white'})
+            .then((dataUrl)=>this.setState({href:dataUrl}).then(console.log(this.state.href)))
+            console.log("smi")
+        }
 
     fetchBox(){       
         const id = '570bad2b45fd40c8197f13a2'  
@@ -49,6 +80,8 @@ class Statistics extends React.Component{
         .then((json)=>this.setState({
             senseBoxData:json,
             loading:false,
+            senseBoxID:json._id,
+            phenomenon:json.sensors[0].title
             }))
        // .then(this.reverseGeocoding)
     }//End fetchbox()
@@ -72,6 +105,58 @@ class Statistics extends React.Component{
         this.fetchBox()
         
     }
+    updateInputPhenom(e){
+        const value = e.target.value
+        this.setState(({
+            phenomenon:value,
+     }))
+    }
+    updateInputId(e){
+        const value = e.target.value
+        this.setState(({
+            senseBoxID:value,
+     }))
+    }
+    updateInputFrom(e){
+        const value = e.target.value
+        this.setState(({
+            from:value,
+     }))
+    }
+    updateInputTo(e){
+        const value = e.target.value
+        this.setState(({
+            to:value,
+     }))
+    }
+    handleClick(){
+        this.Stats.fetchStats()
+            // this.setState({
+            //     clicked:true
+            // })
+    }
+    updateInputWindow(e){
+        const value = e.target.value
+        var window
+        switch(value){
+            case 'Stundenmittelwert':
+                window = 3600000
+                break;
+            case 'Tagesmittelwert':
+                window= 86400000
+                break;
+            case 'Wochenmittelwert':
+                window = 604800000
+                break;
+            default  :
+                window = 3600000
+                break;
+        }
+        console.log(window)
+        this.setState(({
+            window:window,
+     }))
+    }
     render(){
         if(this.state.loading === true ){
             return(
@@ -79,8 +164,45 @@ class Statistics extends React.Component{
             )
         }
         return(
-            
-                <Stats senseBoxData={this.state.senseBoxData}/>
+            <div>
+            <div className="row input-bar">
+            <div className="input-group col-md-12">
+                <span className="input-addon"> <SvgIcon size={20} icon={ic_cloud_queue}/></span>
+                    <select onChange={this.updateInputPhenom} className="form-control" id="sel1">
+                    {this.state.senseBoxData.sensors.map((sensors)=>(
+                                    <option key={sensors._id}>{sensors.title}</option>
+                                ))}
+                    </select>
+            </div>
+                <div className="input-group col-md-12">
+                    <span className="input-addon"> <SvgIcon size={20} icon={ic_router}/></span>
+                    <input type="text" className="form-control" name="senseBoxID" onChange={this.updateInputId} value={this.state.senseBoxID} placeholder="senseBoxID"/><br></br>
+                </div>
+                <div className="input-group col-md-12">
+                    <span className="input-addon"> <SvgIcon size={20} icon={ic_date_range}/></span>
+                    <input className="form-control" type="date" max = {this.state.to} value={this.state.from} name="from" onChange={this.updateInputFrom} placeholder="Start"/>
+                    <input className="form-control" type="date" min = {this.state.from} value={this.state.to} name="to" onChange={this.updateInputTo} placeholder="To"/>
+                </div>
+
+                <div className="input-group col-md-12">
+                        <span className="input-addon"> <SvgIcon size={20} icon={ic_refresh}/></span>
+                        <select onChange={this.updateInputWindow} className="form-control">
+                                <option>Stundenmittelwert</option>
+                                <option>Tagesmittelwert</option>
+                                <option>Wochenmittelwert</option>
+                        </select>
+
+
+                </div>
+                <div className="btn-group col-md-12" >
+                    <button className="button btn btn-sm" type="button" onClick={this.handleClick} value="Apply">Apply Filter</button>
+                    <a className="button" download="story" href={this.state.href}>  
+                                <button style={{width:'100%'}}className="btn btn-sm" value="story"> <SvgIcon size={20} icon={ic_brush}/>Download my Story</button>
+                    </a>
+                    </div>
+            </div> {/* End first row  */}
+            <Stats onRef={ref => (this.Stats = ref )} senseBoxID={this.state.senseBoxID} phenomenon={this.state.phenomenon} lat={this.state.senseBoxData.currentLocation.coordinates[1]} lon={this.state.senseBoxData.currentLocation.coordinates[0]} from={this.state.from} to ={this.state.to} window={this.state.window}/>
+            </div>
             )
     }
 }
--- src/components/Stats.js
+++ src/components/Stats.js
@@ -6,51 +6,37 @@ import {ic_router} from 'react-icons-kit/md/ic_router'
 import {ic_refresh} from 'react-icons-kit/md/ic_refresh'
 import {ic_cloud_queue} from 'react-icons-kit/md/ic_cloud_queue'/* gets called from statistics with props : data */
 import {ic_brush} from 'react-icons-kit/md/ic_brush'
-import domtoimage from 'dom-to-image'
 import Draggable, {DraggableCore} from 'react-draggable'; // Both at the same time
 import { Resizable, ResizableBox } from 'react-resizable';
 
 class Stats extends React.Component{
     constructor(props){
-        var to = new Date()
-        to = to.toISOString().substring(0,10)
-        var from = new Date()
-        from.setMonth(from.getMonth()-1)
-        from = from.toISOString().substring(0,10)
         super(props)
         this.state = {
             loading:false,
             b64image:null,
-            senseBoxID:this.props.senseBoxData._id,
-            phenomenon:this.props.senseBoxData.sensors[0].title,
-            from:from,
-            to:to,
-            window:3600000,
             href:'',
         }
         this.fetchStats = this.fetchStats.bind(this);
-        this.updateInputId = this.updateInputId.bind(this)
-        this.updateInputPhenom = this.updateInputPhenom.bind(this)
-        this.updateInputFrom = this.updateInputFrom.bind(this)
-        this.updateInputTo = this.updateInputTo.bind(this)
-        this.updateInputWindow = this.updateInputWindow.bind(this)
-        this.downloadFile = this.downloadFile.bind(this)
+
+        // this.downloadFile = this.downloadFile.bind(this)
     }
 
     componentDidMount(){
+        this.props.onRef(this)
         this.fetchStats()
     }
-    downloadFile(){
-        var node = document.getElementById('story');
-        domtoimage.toJpeg(document.getElementById('story'), { quality: 0.95 ,bgcolor:'white'})
-            .then((dataUrl)=>this.setState({href:dataUrl}).then(console.log(this.state.href)))
-            console.log("smi")
-        }
+    // downloadFile(){
+    //     var node = document.getElementById('story');
+    //     domtoimage.toJpeg(document.getElementById('story'), { quality: 0.95 ,bgcolor:'white'})
+    //         .then((dataUrl)=>this.setState({href:dataUrl}).then(console.log(this.state.href)))
+    //         console.log("smi")
+    //     }
 
     fetchStats(){
         this.setState({loading:true})
-        var url=this.state.senseBoxID+'/'+this.state.phenomenon+'/'+this.props.senseBoxData.currentLocation.coordinates[1]+'/'+this.props.senseBoxData.currentLocation.coordinates[1]+'/'+this.state.from+'/'+this.state.to+'/'+this.state.window
-        if(this.state.phenomenon==="PM10"){
+        var url=this.props.senseBoxID+'/'+this.props.phenomenon+'/'+this.props.lat+'/'+this.props.lon +'/'+this.props.from+'/'+this.props.to+'/'+this.props.window
+        if(this.props.phenomenon==="PM10"){
             url='python/pm10/'+url
         }
         else{url = 'python/'+url} 
@@ -64,52 +50,7 @@ class Stats extends React.Component{
         })
         .then(()=>this.setState({loading:false})).then(this.downloadFile)
     }
-    updateInputPhenom(e){
-        const value = e.target.value
-        this.setState(({
-            phenomenon:value,
-     }))
-    }
-    updateInputId(e){
-        const value = e.target.value
-        this.setState(({
-            senseBoxID:value,
-     }))
-    }
-    updateInputFrom(e){
-        const value = e.target.value
-        this.setState(({
-            from:value,
-     }))
-    }
-    updateInputTo(e){
-        const value = e.target.value
-        this.setState(({
-            to:value,
-     }))
-    }
-    updateInputWindow(e){
-        const value = e.target.value
-        var window
-        switch(value){
-            case 'Stundenmittelwert':
-                window = 3600000
-                break;
-            case 'Tagesmittelwert':
-                window= 86400000
-                break;
-            case 'Wochenmittelwert':
-                window = 604800000
-                break;
-            default  :
-                window = 3600000
-                break;
-        }
-        console.log(window)
-        this.setState(({
-            window:window,
-     }))
-    }
+
     render(){
         if(this.state.loading){
             return(
@@ -118,42 +59,6 @@ class Stats extends React.Component{
         }
         return(
         <div  className="stats">           
-            <div className="row input-bar">
-                <div className="input-group col-md-12">
-                    <span className="input-addon"> <SvgIcon size={20} icon={ic_cloud_queue}/></span>
-                        <select onChange={this.updateInputPhenom} className="form-control" id="sel1">
-                        {this.props.senseBoxData.sensors.map((sensors)=>(
-                                        <option key={sensors._id}>{sensors.title}</option>
-                                    ))}
-                        </select>
-                </div>
-                    <div className="input-group col-md-12">
-                        <span className="input-addon"> <SvgIcon size={20} icon={ic_router}/></span>
-                        <input type="text" className="form-control" name="senseBoxID" onChange={this.updateInputId} value={this.state.senseBoxID} placeholder="senseBoxID"/><br></br>
-                    </div>
-                    <div className="input-group col-md-12">
-                        <span className="input-addon"> <SvgIcon size={20} icon={ic_date_range}/></span>
-                        <input className="form-control" type="date" max = {this.state.to} value={this.state.from} name="from" onChange={this.updateInputFrom} placeholder="Start"/>
-                        <input className="form-control" type="date" min = {this.state.from} value={this.state.to} name="to" onChange={this.updateInputTo} placeholder="To"/>
-                    </div>
-
-                    <div className="input-group col-md-12">
-                            <span className="input-addon"> <SvgIcon size={20} icon={ic_refresh}/></span>
-                            <select onChange={this.updateInputWindow} className="form-control">
-                                    <option>Stundenmittelwert</option>
-                                    <option>Tagesmittelwert</option>
-                                    <option>Wochenmittelwert</option>
-                            </select>
-
-
-                    </div>
-                    <div className="btn-group col-md-12" >
-                        <button className="button btn btn-sm" type="button" onClick={this.fetchStats} value="Apply">Apply Filter</button>
-                        <a className="button" download="story" href={this.state.href}>  
-                                    <button style={{width:'100%'}}className="btn btn-sm" value="story"> <SvgIcon size={20} icon={ic_brush}/>Download my Story</button>
-                        </a>
-                        </div>
-                </div> {/* End first row  */}
             <div id="story" className="row playground">
                 <Draggable 
                 bounds=''
